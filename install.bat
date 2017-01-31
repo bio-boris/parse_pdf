8dc83380b33d7c5bbdce522d5197be534dea7558
bio-boris <bio-boris@users.noreply.github.com>
2017-01-31 11:24:42 -0600

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,8 @@
+cd C:\parse_pdf-master
+pip install wheel
+pip install lxml-3.6.4-cp27-cp27m-win_amd64.whl
+pip install pypdf
+pip install pypdf2
+pip install lxml
+pip install pdfquery
+pip install reportlab

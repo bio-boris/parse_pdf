f766986bbaea38fcd7918d724d5fa928b7d655b6
bio-boris <sadkhin2@MacBook-Pro.home>
2017-02-18 20:55:42 -0500

    fixes PyPDF2 errors by disabling strict mode

--- pdf_watermark.py
+++ pdf_watermark.py
@@ -7,6 +7,8 @@ from reportlab.lib.pagesizes import letter
 import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
+sys.setrecursionlimit(3000)
+
 
 #Custom Libraries
 from inv import *
@@ -130,7 +132,7 @@ def getWaterMarkedPage(invoice,page):
 	if(len(po_string) < 50):
 		last_y = y_stack[-1]
 		y_stack.append(last_y - 20)
-		
+		can.drawString(x, last_y,		"PO         #: " + po_string)	
 
 	width = 400
 	height = 400
@@ -138,17 +140,16 @@ def getWaterMarkedPage(invoice,page):
 	pixelShift = (len(y_stack) - 3) * 9
 	
 	can.rect(x-10,y_max-60 - pixelShift,width,100+pixelShift)
-	str1 = str(x-10)
-	str2 = str(y_max-60)
-
-	can.drawString(x+50,y_max+50,"X:{0} Y:{1}".format(str1,str2));
+	#str1 = str(x-10)
+	#str2 = str(y_max-60)
+	#can.drawString(x+50,y_max+50,"X:{0} Y:{1}".format(str1,str2));
 
 
 	can.save()
 
 	packet.seek(0)
 
-	watermark = PdfFileReader(packet)
+	watermark = PdfFileReader(packet,strict=False)
 	pp=watermark.getPage(0)
 
 	rotation = page['/Rotate']
@@ -168,7 +169,7 @@ def getWaterMarkedPage(invoice,page):
 
 def printToPDF(pdf_name,invoices):
 	# read your existing PDF
-	existing_pdf = PdfFileReader(file(pdf_name, "rb"))
+	existing_pdf = PdfFileReader(file(pdf_name, "rb"),strict=False)
 	output_file = pdf_name + ".watermarked.pdf"
 	invoices = invoices.getInvoices()
 	# Number of pages in input document
@@ -195,21 +196,22 @@ def printToPDF(pdf_name,invoices):
 		if(complete_flag == True):
 			outputPDF.addPage(watermarked_page)	
 			print "Added COMPLETE PDF page # " + str(p + 1)	+ " (Page " + str(actual_page) + ")"
+			sys.stdout.flush()
 			actual_page+=1;
-
-		#Don't move pages that don't have invoice, assume they are placeholder pages
-		elif(complete_flag == False and invoice.unknown_invoice == True):
-			outputPDF.addPage(watermarked_page)	
-			print "Added NON INVOICE PDF page # " + str(p + 1)+" (Page " + str(actual_page) + ")"
-			actual_page+=1;
-		#Save the PDF page for printing later	
 		else:
 			incomplete_pages.append([watermarked_page,p])
-		sys.stdout.flush()
-
+		'''			
+		#Don't move pages that don't have invoice, assume they are placeholder pages
+				elif(complete_flag == False and invoice.unknown_invoice == True):
+					outputPDF.addPage(watermarked_page)	
+					print "Added NON INVOICE PDF page # " + str(p + 1)+" (Page " + str(actual_page) + ")"
+					actual_page+=1;
+				#Save the PDF page for printing later	
+				else:
+					incomplete_pages.append([watermarked_page,p])
+		'''
 
 	#Add incomplete pages
-
 	for page in incomplete_pages:
 		outputPDF.addPage(page[0]);
 		print "Added [missing PO or JOB#] PDF page # " + str(page[1] + 1) + " (Page " + str(actual_page) + ")"
@@ -230,7 +232,7 @@ def printToPDF(pdf_name,invoices):
 
 def printToPDF_multiple(pdf_name,invoices):
 	# read your existing PDF
-	existing_pdf = PdfFileReader(file(pdf_name, "rb"))
+	existing_pdf = PdfFileReader(file(pdf_name, "rb"),strict=False)
 	output_file = pdf_name + ".watermarked.pdf"
 	output_file_incomplete = pdf_name + ".watermarked.incomplete.pdf"
 	output_file_missing = pdf_name + ".watermarked.missing.pdf"
